2b657e34c3416f876d1ee2e21114ab8ab1c5fbbb
Gokul <104612046+gokul1414@users.noreply.github.com>
2023-02-27 11:27:07 +0530

    Update settings.py

--- informationWeb/settings.py
+++ informationWeb/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 # EMAIL CONFIGURATION (for contact form)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'gokulksrce@gmail.com'
-EMAIL_HOST_PASSWORD = 'inwupbbcmpxnllqi'
+EMAIL_HOST_USER = '<Yourmail ID>'
+EMAIL_HOST_PASSWORD = 'Your SMTP Password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
